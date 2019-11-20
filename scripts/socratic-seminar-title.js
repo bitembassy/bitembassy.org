c65d807d6b2aeb76f732c679877ceac6469fa645
Nadav Ivgi <nadav@shesek.info>
2019-11-20 10:49:25 +0200

    Automatically infer title for socratic seminar pages

---
+++ scripts/socratic-seminar-title.js
@@ -0,0 +1,14 @@
+const RE_TITLE = /^seminar-(\d+)$/
+
+const hasCategory = (data, category) =>
+  data.categories.some(cat => cat.name == category)
+
+hexo.extend.filter.register('before_post_render', data => {
+  if (data.layout === 'post' && hasCategory(data, 'socratic')) {
+    const matches = data.title.match(RE_TITLE)
+    if (matches) {
+      data.title = `Socratic Seminar ${matches[1]} - ${data.date.format('MMM Do YYYY')}`
+    }
+  }
+  return data
+})
