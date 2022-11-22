f3de66e329b0a9a3e7e0ebdd049a1dc91da08c55
sha-265 <4103710+sha-265@users.noreply.github.com>
2022-11-23 00:32:20 +0200

    Site updated: 2022-11-23 00:32:20

---
+++ js/script.js
@@ -0,0 +1,30 @@
+(function($){
+  // Mobile nav
+  var $container = $('#page'),
+    isMobileNavAnim = false,
+    mobileNavAnimDuration = 200;
+
+  var startMobileNavAnim = function(){
+    isMobileNavAnim = true;
+  };
+
+  var stopMobileNavAnim = function(){
+    setTimeout(function(){
+      isMobileNavAnim = false;
+    }, mobileNavAnimDuration);
+  }
+
+  $('#main-nav-toggle').on('click', function(){
+    if (isMobileNavAnim) return;
+
+    startMobileNavAnim();
+    $container.toggleClass('mobile-nav-on');
+    stopMobileNavAnim();
+  });
+
+  $('#wrap').on('click', function(){
+    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
+
+    $container.removeClass('mobile-nav-on');
+  });
+})(jQuery);
